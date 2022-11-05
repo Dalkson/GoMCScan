c2b97fadc0d9c2e5391372692b672c8d584596d3
Dalkson <jacksondpope@gmail.com>
2022-11-04 21:10:51 -0400

    removed unneeded structs and port decleration

--- main.go
+++ main.go
@@ -12,33 +12,12 @@ import (
 	"github.com/aherve/gopool"
 )
 
-type PingResponse struct {
-	//Latency     uint // Latency between you and the server
-	//PlayerCount PlayerCount // Players count information of the server
-	//Protocol    int // Protocol number of the server
-	//Favicon     string // Favicon in base64 of the server
-	Motd    string         // Motd of the server without color
-	Version string         // Version of the server
-	Sample  []PlayerSample // List of connected players on the server
-}
-
-type PlayerCount struct {
-	Online int // Number of connected players on the server
-	Max    int // Number of maximum players on the server
-}
-
-type PlayerSample struct {
-	UUID string // UUID of the player
-	Name string // Name of the player
-}
-
 func main() {
 	threads, _ := strconv.Atoi(os.Args[1])
 	pool := gopool.NewPool(int(threads))
 	g := 176
 	h := 9
-	var port uint16
-	port = 25565
+	var port uint16 = 25565
 
 	for j := 0; j < 255; j++ {
 		for k := 0; k < 255; k++ {
