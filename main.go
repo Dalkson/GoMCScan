1b7ec34b7eb5ad82ba1d0a742f35b5bdcece1f8c
Alexander Bass <alexander@alexanderbass.com>
2022-11-07 21:53:56 -0500

    remove needless if statement

--- main.go
+++ main.go
@@ -94,10 +94,8 @@ func pingIt(ip string, port uint16) {
 		printStatus(fmt.Sprintf("%v:%v | %v  online | %v", ip, port, data.PlayerCount.Online, data.Motd))
 		formatted := formattedOutput{time.Now().Format("2006-01-02 15:04:04"), ip+":"+fmt.Sprint(port), data.Version, data.Motd, data.PlayerCount, data.Sample}
 		record(formatted)
-		if conf.saveFavicon {
-			if data.Favicon != "" {
+		if conf.saveFavicon && data.Favicon != "" {
 				saveFavicon(data.Favicon, ip, port)
-			}
 		}
 	}
 }
